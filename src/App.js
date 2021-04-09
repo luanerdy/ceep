bca6394647b71b29b8c0fb69259fb771ce97e9b8
luanerdy <luan.vini1@gmail.com>
2021-04-08 21:52:31 -0300

    Componente lista de notas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import ListaDeNotas from './components/listaDeNotas';
 
 function App() {
   return (
-    <h1>Meu app react</h1>
+    <section>
+      <form>
+        <input type="text" placeholder="Título"/>
+        <textarea placeholder="Escreva sua nota...">
+        </textarea>
+        <button>Criar Nota</button>
+      </form>
+      <ListaDeNotas />
+    </section>
   );
 }
 
